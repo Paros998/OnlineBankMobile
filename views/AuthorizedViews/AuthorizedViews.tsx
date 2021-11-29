ec8b91fe7b01ccb9e847f865cae413a86802008c
Dominik Grudzień <62188218+shanodis@users.noreply.github.com>
2021-11-29 23:57:28 +0100

    Added new views structure

--- views/AuthorizedViews/AuthorizedViews.tsx
+++ views/AuthorizedViews/AuthorizedViews.tsx
@@ -1,17 +1,58 @@
 import React from 'react';
-
 import Home from "./Home/Home";
 import Payments from "./Payments/Payments";
-import Transfers from "./Payments/Transfers";
+import { BottomTabNavigationOptions, createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AiOutlineHome } from "react-icons/ai";
+import { FaRegMoneyBillAlt } from "react-icons/fa";
+import { BsPerson } from "react-icons/bs";
+import Account from "./Account/Account";
 
+const Tab = createBottomTabNavigator();
+
+const navigatorOptions: BottomTabNavigationOptions = {
+  headerShown: false,
+  tabBarLabelStyle: { fontSize: 14, fontWeight: 'bold' },
+  tabBarLabelPosition: 'below-icon',
+  tabBarActiveTintColor: 'red',
+  tabBarInactiveTintColor: 'black'
+};
+
+const homeScreenOptions: BottomTabNavigationOptions = {
+  tabBarIcon: ({ size }) => <AiOutlineHome size={size + 5}/>,
+};
+
+const paymentsScreenOptions: BottomTabNavigationOptions = {
+  tabBarIcon: ({ size }) => <FaRegMoneyBillAlt size={size + 5}/>,
+};
+
+const accountScreenOptions: BottomTabNavigationOptions = {
+  tabBarIcon: ({ size }) => <BsPerson size={size + 5}/>,
+};
 
 const AuthorizedViews = () => {
   return (
-    <>
-      <Stack.Screen name="Payments" component={Payments}/>
-      <Stack.Screen name="HomePage" component={Home}/>
-      <Stack.Screen name="Transfers" component={Transfers}/>
-    </>
+    <Tab.Navigator
+      initialRouteName='Home'
+      screenOptions={navigatorOptions}
+    >
+      <Tab.Screen
+        name='Home'
+        component={Home}
+        options={homeScreenOptions}
+      />
+
+      <Tab.Screen
+        name='Payments'
+        component={Payments}
+        options={paymentsScreenOptions}
+      />
+
+      <Tab.Screen
+        name='Account'
+        component={Account}
+        options={accountScreenOptions}
+      />
+    </Tab.Navigator>
   );
 };
 
